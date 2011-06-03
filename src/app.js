b455a0294543ac234fb1f55b1c954341e46f6cb1
Jan Javorek <honza@javorek.net>
2011-06-03 13:12:55 +0200

    added option to search direct phrase (I am feeling lucky)

--- src/app.js
+++ src/app.js
@@ -21,11 +21,25 @@ $(document).ready(function() {
 	/* Search form */
 	$('form').submit(function(e) {
 		e.preventDefault();
+		var video_title = $('[type="search"]', this).val();
 		
-		$('section').empty().hide();
-		
-		var film_title = $('[type="search"]', this).val();
-		getRemote('http://www.csfd.cz/hledat/?q=' + encodeURIComponent(film_title), getCsfd);
+		if (video_title.length) {
+			$('section').empty().hide();
+			
+			if ($('#feeling-lucky', this).prop('checked')) {
+				selectVideo({
+					url: null,
+					title: video_title,
+					titles: [video_title],
+					desc: 'Přímá fráze z vyhledávání.',
+					ranking: 0,
+					rating: 0
+				});
+				
+			} else {
+				getRemote('http://www.csfd.cz/hledat/?q=' + encodeURIComponent(video_title), getCsfd);
+			}
+		}
 	});
 	
 	/* CSFD */
@@ -58,7 +72,7 @@ $(document).ready(function() {
 				ranking = 1;
 			}
 			
-			li = $('<li class="film rank-' + ranking + '"><h3>' + title + ' <a href="' + url + '" target="_blank">ČSFD</a></h3><small>' + desc + '</small><button>✔</button></li>');
+			li = $('<li class="video rank-' + ranking + '"><h3>' + title + ' <a href="' + url + '" target="_blank">ČSFD</a></h3><small>' + desc + '</small><button>✔</button></li>');
 			li.data('film', {url: url, title: title, desc: desc, ranking: ranking});
 			li.appendTo(ul);
 		});
@@ -99,7 +113,7 @@ $(document).ready(function() {
 			ranking = 1;
 		}
 		
-		selectFilm({
+		selectVideo({
 			url: 'http://www.csfd.cz' + $('.navigation a[href^="/film"]', response).attr('href').replace(/(\/film\/[^\/]+\/).*$/, '$1'),
 			title: $('.info h1', response).text(),
 			titles: titles,
@@ -109,20 +123,21 @@ $(document).ready(function() {
 		});
 	};
 	
-	/* Film selection */
-	var selectFilm = function(film) {
+	/* Video selection */
+	var selectVideo = function(video) {
 		$('.films').hide();
 		section = $('.results');
 		
-		header = $('<header id="selected-film" class="film rank-' + film.ranking + '"><h3>' + film.title + ' <a href="' + film.url + '" target="_blank">ČSFD</a></h3><small>' + film.desc + '</small></header>');
-		header.data('film', film);
+		link = (video.url)? ' <a href="' + video.url + '" target="_blank">ČSFD</a>' : '';
+		header = $('<header id="selected" class="video rank-' + video.ranking + '"><h3>' + video.title + link + '</h3><small>' + video.desc + '</small></header>');
+		header.data('video', video);
 		section.append(header);
 		
 		section.append('<h2>The Pirate Bay</h2><ul id="tpb"></ul>');
-		askTpb(film);
+		askTpb(video);
 		
 		section.append('<h2>Ulož.to</h2><ul id="ulozto"></ul>');
-		askUlozto(film);
+		askUlozto(video);
 		
 		section.append('<h2>Titulky.com</h2><ul id="titulkycom"></ul>');
 
@@ -130,18 +145,18 @@ $(document).ready(function() {
 	};
 	
 	/* Uloz.to */
-	var askUlozto = function(film) {
-		for (var i = 0; i < film.titles.length; i++) {
-			var title = film.titles[i];
+	var askUlozto = function(video) {
+		for (var i = 0; i < video.titles.length; i++) {
+			var title = video.titles[i];
 			$('#ulozto').append('<li>→ <a href="http://www.uloz.to/hledej/?category=2&type=ratings&q=' + encodeURIComponent(title) + '" target="_blank">' + title + '</a></li>');
 		}
 	};
 	
 	/* The Pirate Bay */
-	var askTpb = function(film) {
-		var counter = film.titles.length;
-		for (var i = 0; i < film.titles.length; i++) {
-			var title = film.titles[i];
+	var askTpb = function(video) {
+		var counter = video.titles.length;
+		for (var i = 0; i < video.titles.length; i++) {
+			var title = video.titles[i];
 			getRemote('http://thepiratebay.org/search/' + encodeURIComponent(title) + '/0/7/200', function(response) {
 				$('.detName', $(response.responseText)).each(function(i, item) {
 					if (i > 4) {
@@ -159,16 +174,16 @@ $(document).ready(function() {
 				});
 				
 				if (!--counter) {
-					askTitulkycom(film); // to be sure TPB is loaded already!
+					askTitulkycom(video); // to be sure TPB is loaded already!
 				}
 			});
 		}
 	};
 	
 	/* Titulky.com */
-	var askTitulkycom = function(film) {
-		for (var i = 0; i < film.titles.length; i++) {
-			var title = film.titles[i];
+	var askTitulkycom = function(video) {
+		for (var i = 0; i < video.titles.length; i++) {
+			var title = video.titles[i];
 			var searchUrl = 'http://www.titulky.com/index.php?orderby=-9&Fulltext=' + encodeURIComponent(title);
 			//$('#titulkycom').append('<li>→ <a href="' + searchUrl + '" target="_blank">' + title + '</a></li>');
 			
